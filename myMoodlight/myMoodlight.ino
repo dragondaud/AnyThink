e5551a6685c6413f00e62931c863bd8390d00009
daud <26858302+dragondaud@users.noreply.github.com>
2025-07-25 20:13:08 -0600

    Update myMoodlight.ino
    comments

--- myMoodlight/myMoodlight.ino
+++ myMoodlight/myMoodlight.ino
@@ -1,3 +1,7 @@
+// myMoodlight
+// by David Denney <dragondaud@gmail.com>
+// based on project https://www.instructables.com/Mood-Light-That-Matches-Any-Color-You-Tap-on-the-A/
+
 #include <FastLED.h>  // https://fastled.io/
 
 #define LED_PIN 6         // D6 (PA07) for WS2812 LEDs
@@ -13,7 +17,6 @@
 #define TOUCH_PIN_3 3  // D3 (PA11)
 #define TOUCH_PIN_4 4  // D4 (PA08)
 
-// color names https://github.com/FastLED/FastLED/blob/master/src/crgb.h#L557
 // Colors using RGB values directly, instead of predefined colors
 const CRGB COLOR_GREEN = CRGB(0, 204, 0);      // RGB: (0, 204, 0) - Very saturated green
 const CRGB COLOR_BLUE = CRGB(0, 102, 204);     // RGB: (0, 102, 204) - Much more saturated blue
@@ -24,25 +27,28 @@ const CRGB COLOR_VIOLET = CRGB(128, 0, 255);   // RGB: (128, 0, 255) - More satu
 // Define the LED array
 CRGB leds[LED_COUNT];
 
-// Define colors for each button, 0 - 4, in order
+// Define colors for each button, 0 - 4, in order from predefined colors or
+// color names https://github.com/FastLED/FastLED/blob/master/src/crgb.h#L557
 CRGB ledColors[] = { COLOR_YELLOW, COLOR_VIOLET, COLOR_RED, COLOR_GREEN, COLOR_BLUE };
 
 // Selected color and previous color
 int Color, lastColor;
 
 void setup() {
-  //Initialize serial and wait for port to open
+  // Initialize serial and wait for port to open
+  // https://docs.arduino.cc/language-reference/en/functions/communication/serial/
   Serial.begin(9600);
-  while (!Serial) { delay(250); }
+  while (!Serial) { delay(250); }  // wait for serial port to initialize
   Serial.println("myMoodlight Serial Open");
 
-  // Initialize FastLED
+  // Initialize FastLED library
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, LED_COUNT);
   FastLED.setBrightness(BRIGHTNESS);
   FastLED.clear();
   FastLED.show();
 
   // Set up touch sensor pins as inputs
+  // https://docs.arduino.cc/language-reference/en/functions/digital-io/pinMode/
   pinMode(TOUCH_PIN_0, INPUT);
   pinMode(TOUCH_PIN_1, INPUT);
   pinMode(TOUCH_PIN_2, INPUT);
@@ -58,12 +64,15 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(TOUCH_PIN_4), handle_4, RISING);
 
   // Small delay for stability
+  // https://docs.arduino.cc/language-reference/en/functions/time/delay/
   delay(50);
 
   // Display a default pattern
   showDefaultPattern();
 }
 
+
+// Interrupt handlers for each touch pin
 void handle_0() {
   Color = 0;
 }
@@ -85,17 +94,23 @@ void handle_4() {
 }
 
 void loop() {
+  // If selected Color has changed, update led display, remember selection
   if (Color != lastColor) {
     lastColor = Color;
     Serial.println(Color);
-    fill_solid(leds, LED_COUNT, ledColors[Color]);
-    FastLED.show();
+    fill_solid(leds, LED_COUNT, ledColors[Color]);  // fill array with Color
+    FastLED.show();                                 // display array on leds
+    delay(50);
+  }
+  if (Serial.available()) {
+    Serial.readString();
+    showDefaultPattern();  // reset to default pattern on serial input
     delay(50);
   }
 }
 
 void showDefaultPattern() {
-  int splitCount = LED_COUNT / 5;
+  int splitCount = LED_COUNT / 5;  // split available leds into 5 sections
   Serial.println("Show Default Pattern");
   fill_solid(leds, splitCount, COLOR_RED);
   fill_solid(leds + (splitCount * 1), splitCount, COLOR_VIOLET);
