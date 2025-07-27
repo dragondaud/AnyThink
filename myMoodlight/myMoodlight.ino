ac9104d83d4d30a5f9a882f9df93fbe4ff689ffd
daud <26858302+dragondaud@users.noreply.github.com>
2025-07-26 20:09:46 -0600

    Update myMoodlight.ino
    cleanups and comments

--- myMoodlight/myMoodlight.ino
+++ myMoodlight/myMoodlight.ino
@@ -10,7 +10,7 @@
 #define LED_TYPE WS2812B  // Type of LED pixels
 #define COLOR_ORDER GRB   // Color order
 
-// Input pins
+// Touch sensor input pins
 #define TOUCH_PIN_0 0  // D0 (PA02)
 #define TOUCH_PIN_1 1  // D1 (PA04)
 #define TOUCH_PIN_2 2  // D2 (PA10)
@@ -20,9 +20,10 @@
 // Define the LED array
 CRGB leds[LED_COUNT];
 
-// Define colors for each button, 0 - 4, in order, from predefined color names or triad as CRGB(200, 200, 200)
+// Define array of colors, for each button, 1 - 5, and 0 for default pattern
+// from predefined color names or triad as CRGB(200, 200, 200)
 // color names https://github.com/FastLED/FastLED/blob/master/src/crgb.h#L557
-CRGB ledColors[] = { CRGB::Yellow, CRGB::DarkViolet, CRGB::DarkRed, CRGB::DarkGreen, CRGB::DarkBlue };
+CRGB ledColors[] = { CRGB(0, 0, 0), CRGB::Yellow, CRGB::DarkViolet, CRGB::DarkRed, CRGB::DarkGreen, CRGB::DarkBlue };
 
 // Selected color and previous color
 int Color, lastColor;
@@ -30,14 +31,14 @@ int Color, lastColor;
 // track double taps
 const unsigned long DOUBLE_TAP_TIMEOUT = 1500;  // milliseconds
 unsigned long firstTapTime = 0;
-bool tap;
+bool tap = false;
 
 void setup() {
-  // Initialize serial and wait for port to open
+  // Initialize serial and wait for port to open, for debugging
   // https://docs.arduino.cc/language-reference/en/functions/communication/serial/
   Serial.begin(9600);
-  delay(2000);  // wait for serial port to initialize
-  Serial.println("myMoodlight Serial Open");
+  delay(2000);
+  Serial.println("myMoodlight Ready");
 
   // Initialize FastLED library
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, LED_COUNT);
@@ -55,11 +56,11 @@ void setup() {
 
   // configure interrupts
   // https://docs.arduino.cc/language-reference/en/functions/external-interrupts/attachInterrupt/
-  attachInterrupt(digitalPinToInterrupt(TOUCH_PIN_0), handle_0, RISING);
-  attachInterrupt(digitalPinToInterrupt(TOUCH_PIN_1), handle_1, RISING);
-  attachInterrupt(digitalPinToInterrupt(TOUCH_PIN_2), handle_2, RISING);
-  attachInterrupt(digitalPinToInterrupt(TOUCH_PIN_3), handle_3, RISING);
-  attachInterrupt(digitalPinToInterrupt(TOUCH_PIN_4), handle_4, RISING);
+  attachInterrupt(digitalPinToInterrupt(TOUCH_PIN_0), handle_1, RISING);
+  attachInterrupt(digitalPinToInterrupt(TOUCH_PIN_1), handle_2, RISING);
+  attachInterrupt(digitalPinToInterrupt(TOUCH_PIN_2), handle_3, RISING);
+  attachInterrupt(digitalPinToInterrupt(TOUCH_PIN_3), handle_4, RISING);
+  attachInterrupt(digitalPinToInterrupt(TOUCH_PIN_4), handle_5, RISING);
 
   // Small delay for stability
   // https://docs.arduino.cc/language-reference/en/functions/time/delay/
@@ -69,12 +70,8 @@ void setup() {
   showDefaultPattern();
 }
 
-// Interrupt handlers for each touch pin
-void handle_0() {
-  Color = 0;
-  tap = true;
-}
-
+// Interrupt handlers for each touch pin and unhandled tap tracking
+// For more information on interrupts: https://gammon.com.au/interrupts
 void handle_1() {
   Color = 1;
   tap = true;
@@ -95,40 +92,53 @@ void handle_4() {
   tap = true;
 }
 
+void handle_5() {
+  Color = 5;
+  tap = true;
+}
+
 void loop() {
   // If selected Color has changed, update led display, remember selection
   if (Color != lastColor) {
     lastColor = Color;
+
+    // output touch color to serial, for debugging
     uint8_t r = ledColors[Color].r;
     uint8_t g = ledColors[Color].g;
     uint8_t b = ledColors[Color].b;
     Serial.printf("Touch#%d: (%d, %d, %d)\n", Color, r, g, b);
+
     fill_solid(leds, LED_COUNT, ledColors[Color]);  // fill array with Color
     FastLED.show();                                 // display array on leds
-    tap = false;
-    firstTapTime = millis();
-    delay(50);
+    tap = false;                                    // reset tap tracking
+    firstTapTime = millis();                        // track tap time
+    delay(50);                                      // prevent bounce
+
   } else if (Color == lastColor and tap == true) {  // new tap same color
-    if (millis() - firstTapTime < DOUBLE_TAP_TIMEOUT) {
-      Serial.println("double tap");
+
+    if (millis() - firstTapTime < DOUBLE_TAP_TIMEOUT) {  // second tap within timeout
+      Serial.println("Double tap");
       showDefaultPattern();
     }
+
     tap = false;
   }
-  if (Serial.available()) {
-    Serial.readString();
-    showDefaultPattern();  // reset to default pattern on serial input
+
+  if (Serial.available()) {  // check Serial input buffer
+    Serial.readString();     // read string to eol or timeout
+    showDefaultPattern();    // reset to default pattern on any serial input
     delay(50);
   }
 }
 
 void showDefaultPattern() {
   int splitCount = LED_COUNT / 5;  // split available leds into 5 sections
-  Serial.println("Show Default Pattern");
+  Serial.println("Show default pattern");
   fill_solid(leds, splitCount, ledColors[0]);
   fill_solid(leds + (splitCount * 1), splitCount, ledColors[1]);
   fill_solid(leds + (splitCount * 2), splitCount, ledColors[2]);
   fill_solid(leds + (splitCount * 3), splitCount, ledColors[3]);
   fill_solid(leds + (splitCount * 4), splitCount, ledColors[4]);
   FastLED.show();
+  Color = 0;
 }
